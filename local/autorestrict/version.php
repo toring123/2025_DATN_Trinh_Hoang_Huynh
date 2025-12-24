b063fb4702dcb080a7f813f77ce34efb56aeb4b3
hoangduy1612 <duy031612@gmail.com>
2025-12-24 22:25:10 +0700

    Feat: Delete redundant code

--- local/autorestrict/version.php
+++ local/autorestrict/version.php
@@ -10,7 +10,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_autorestrict';
-$plugin->version = 2025120401;
+$plugin->version = 2025120600;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.1.1';
